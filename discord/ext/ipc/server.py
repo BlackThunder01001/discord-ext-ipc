7615fd1d2ecff324a8cd23a7ae5b0c08c45a1a94
AwesomeSam9523 <76945339+AwesomeSam9523@users.noreply.github.com>
2021-11-05 13:53:10 +0530

    Update server.py

--- discord/ext/ipc/server.py
+++ discord/ext/ipc/server.py
@@ -271,6 +271,5 @@ class Server:
             self._multicast_server = aiohttp.web.Application()
             self._multicast_server.router.add_route("GET", "/", self.handle_multicast)
 
-            asyncio.create_task(self.__start(self._multicast_server, self.multicast_port))
-
-        asyncio.create_task(self.__start(self._server, self.port))
+            self.loop.run_until_complete(self.__start(self._multicast_server, self.multicast_port))
+        self.loop.run_until_complete(self.__start(self._server, self.port))

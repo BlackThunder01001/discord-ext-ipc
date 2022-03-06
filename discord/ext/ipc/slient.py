c8a80004424ff12b019b4ddc39ff4594b2f1572e
Black Thunder <40216575+BlackThunder01001@users.noreply.github.com>
2022-03-06 22:23:19 +0500

    Update slient.py

--- discord/ext/ipc/slient.py
+++ discord/ext/ipc/slient.py
@@ -2,6 +2,6 @@ from .client import Client
 from .server import Server
 
 class Slient(Client, Server):
-    def __init__(*args, **kwargs):
-        Client.__init__(self, *args, **kwargs)
-        Server.__init__(self, *args, **kwargs)
+    def __init__(self, port=8765, *args, **kwargs):
+        Client.__init__(self, port=port, *args, **kwargs)
+        Server.__init__(self, port=port+1, *args, **kwargs)

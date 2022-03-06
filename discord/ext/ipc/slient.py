63aa11e9103d401326ba9b8b1430255531f75c89
Black Thunder <40216575+BlackThunder01001@users.noreply.github.com>
2022-03-06 22:37:53 +0500

    Add multicast port

--- discord/ext/ipc/slient.py
+++ discord/ext/ipc/slient.py
@@ -9,17 +9,21 @@ class Slient(Client, Server):
         sending_port = 8654,
         receiving_port = 8653,
         secret_key = None,
+        sending_multicast = 20000,
+        receiving_multicast = 20001,
     ):
         Client.__init__(
             self,
             port = receiving_port,
+            multicast_port = receiving_multicast,
             host = host,
             secret_key = secret_key)
-            
+
         Server.__init__(
             self,
             bot = bot,
             port = sending_port,
+            multicast_port = sending_multicast,
             host = host,
             secret_key = secret_key
         )

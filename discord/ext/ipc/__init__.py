f395b96d197c62b2e26a5cf84fcfd22a981d848b
Black Thunder <40216575+BlackThunder01001@users.noreply.github.com>
2022-03-06 22:22:41 +0500

    Update __init__.py

--- discord/ext/ipc/__init__.py
+++ discord/ext/ipc/__init__.py
@@ -2,10 +2,11 @@ import collections
 
 from discord.ext.ipc.client import Client
 from discord.ext.ipc.server import Server
+from discord.ext.ipc.slient import Slient
 from discord.ext.ipc.errors import *
 
 
 _VersionInfo = collections.namedtuple("_VersionInfo", "major minor micro release serial")
 
-version = "2.1.1"
-version_info = _VersionInfo(2, 1, 1, "final", 0)
+version = "2.1.2"
+version_info = _VersionInfo(2, 1, 2, "final", 0)
